138069631d7eadc215cf2e8e40d5bb1c47ba2a77
Shisato <shisatoyano@gmail.com>
2020-12-18 22:24:39 +0900

    implemented find most frequent count element in array

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 * If there are multiple elements that appear maximum number of times, print any one of them.  
 * To do sorting is better than to run two loops.  
 * First sort the array, then linearly traverse the array.  
+* Using hashing is more efficient.  
+* Store elements and their frequency counts as key value pairs.  
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int most_frequent(int array[], int n) {
+    // insert all elements in hash
+    unordered_map<int, int> hash;
+    for (int i = 0; i < n; ++i) {
+        hash[array[i]]++;
+    }
+
+    // find the max frequency
+    int max_count = 0, res = -1;
+    for (auto i: hash) {
+        if (max_count < i.second) {
+            res = i.first;
+            max_count = i.second;
+        }
+    }
+
+    return res;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int array[] = {1, 5, 2, 1, 3, 2, 1};
+
+    int n = sizeof(array) / sizeof(array[0]);
+
+    cout << most_frequent(array, n);
+
     return 0;
 }
